17ed4e678be6dc2ad841b1776ac75903bdc8e4e5
Navid <Navidjalili44@gmail.com>
2022-12-24 20:12:31 +0330

    Refactor base code

--- script.py
+++ script.py
@@ -1,59 +1,44 @@
+import sys
+import logging
 from typing import List
 from bs4 import BeautifulSoup
 from requests import get
+from constants.request_config import URL, HEADERS
 from functions.validate_input import isValidWord
+from functions.format_word_meaning import get_word_meaning
+from functions.format_word_example import get_word_example
+from functions.save_word import save_word_to_file
 
-word = input('Enter the word you\'re looking for:\n')
+logging.basicConfig(filename="log.txt", level=logging.ERROR)
 
-if isValidWord(word):
-    url = f'https://dictionary.cambridge.org/dictionary/english/{word.strip()}'
 
-    # Fake Google Chrome
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+if __name__ == '__main__':
+    word = input('Enter the word you\'re looking for:\n')
 
-    response_word = get(url, headers=headers)
+    if isValidWord(word):
+        response_word = get(URL + word, headers=HEADERS)
 
-    # Meaning of the searched word
-    word_meaning: str = ''
+        # Meaning of the searched word
+        # An example of the searched word in a sentence
+        word_meaning = ''
+        word_example = ''
 
-    # An example of the searched word in a sentence
-    word_example: str = ''
+        try:
+            soup = BeautifulSoup(response_word.text, "html5lib")
 
-    # with open('data.html', mode='r', encoding="utf-8") as html_file:
-    # html_file.write(response_word.text)
-    soup = BeautifulSoup(response_word.text, "html5lib")
+            response_word_meaning_block = soup.find(
+                'div', {'class': 'def ddef_d db'}).children
 
-    response_word_meaning_block = soup.find(
-        'div', {'class': 'def ddef_d db'}).children
+            response_word_example_block = soup.find(
+                'div', {'class': 'examp dexamp'}).children
+        except Exception as err:
+            logging.error(
+                f'\n\nSomething went wrong, please check the api.\n\n')
+            print('Something went wrong.')
+            sys.exit(1)
 
-    response_word_example = soup.find(
-        'div', {'class': 'examp dexamp'}).children
+        word_meaning = f"{word} - {get_word_meaning(response_word_meaning_block)} - {get_word_example(response_word_example_block)}"
 
-    word_meaning_indiviual_words: List[str] = []
-    word_example_indiviual_words: List[str] = []
-
-    for i in response_word_meaning_block:
-        word_meaning_indiviual_words.append(i.text)
-
-    for j in response_word_example:
-        word_example_indiviual_words.append(j.text)
-
-    # Remove the last colon
-    word_meaning_indiviual_words.pop()
-
-    # Save it in the format: word - meaning - "example"
-    word_example_indiviual_words.insert(0, "\"")
-    word_example_indiviual_words.append("\"")
-
-    word_meaning = f"{word} - " + ''.join(
-        word_meaning_indiviual_words) + ' - ' + ''.join(word_example_indiviual_words)
-
-    with open('words.txt', mode='a', encoding="utf-8") as my_vocab_list:
-        my_vocab_list.write('\n')
-        my_vocab_list.write('\n')
-        my_vocab_list.write(word_meaning)
-
-        print(f'{word} was successfully added to {my_vocab_list.name}')
-else:
-    print('Please provide a valid word.')
+        save_word_to_file(word, word_meaning)
+    else:
+        print('Please provide a valid word. (Avoid using numbers or special characters)')
